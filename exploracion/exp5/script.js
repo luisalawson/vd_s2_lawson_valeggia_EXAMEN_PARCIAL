165744302a4b1c3107ed72885e9ab852534e3765
luisalawson <luisalawson14@gmail.com>
2023-04-12 11:40:24 -0300

    initial commit

--- exploracion/exp5/script.js
+++ exploracion/exp5/script.js
@@ -1,15 +1,12 @@
-// config. fecha español
-d3.json('https://cdn.jsdelivr.net/npm/d3-time-format@3/locale/es-ES.json').then(locale => {
-  d3.timeFormatDefaultLocale(locale)
-})
-
 const mapaFetch = d3.json('barrios-caba.geojson')
 const dataFetch = d3.csv('autos_2.csv', d3.autoType)
 
 Promise.all([mapaFetch, dataFetch]).then(([barrios, data]) => {
   
+  /* Agrupamos reclamos x barrio */
   const reclamosPorBarrio = d3.group(data, d => d.domicilio_barrio) // crea un Map
   console.log('reclamosPorBarrio', reclamosPorBarrio)
+  
   /* Mapa Coroplético */
   let chartMap = Plot.plot({
     // https://github.com/observablehq/plot#projection-options
@@ -36,14 +33,6 @@ Promise.all([mapaFetch, dataFetch]).then(([barrios, data]) => {
         title: d => `${d.properties.BARRIO}\n${d.properties.DENUNCIAS} denuncias`,
       }),
     ],
-    facet: {
-      data: data,
-      x: d => d3.timeFormat('%a')(d3.timeParse('%a/%d/%y')(d.hora_ingreso)),
-    },
-    fx: {
-     // domain: ['lun', 'mar', 'mié', 'jue', 'vie', 'sáb', 'dom']
-    },
-    width: 1000
   })
 
   /* Agregamos al DOM la visualización chartMap */
