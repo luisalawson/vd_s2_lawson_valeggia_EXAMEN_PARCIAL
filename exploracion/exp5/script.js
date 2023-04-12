b87ac7628433510ba0a5095dfc0c0181593c8bd8
luisalawson <luisalawson14@gmail.com>
2023-04-12 11:28:01 -0300

    initial commit

--- exploracion/exp5/script.js
+++ exploracion/exp5/script.js
@@ -4,10 +4,12 @@ d3.json('https://cdn.jsdelivr.net/npm/d3-time-format@3/locale/es-ES.json').then(
 })
 
 const mapaFetch = d3.json('barrios-caba.geojson')
-const dataFetch = d3.dsv(';','todos.csv', d3.autoType)
+const dataFetch = d3.csv('autos_2.csv', d3.autoType)
 
 Promise.all([mapaFetch, dataFetch]).then(([barrios, data]) => {
   
+  const reclamosPorBarrio = d3.group(data, d => d.domicilio_barrio) // crea un Map
+  console.log('reclamosPorBarrio', reclamosPorBarrio)
   /* Mapa Coroplético */
   let chartMap = Plot.plot({
     // https://github.com/observablehq/plot#projection-options
@@ -16,20 +18,31 @@ Promise.all([mapaFetch, dataFetch]).then(([barrios, data]) => {
       domain: barrios, // Objeto GeoJson a encuadrar
     },
     color: {
+      // Quantize continuo (cant. denuncias) -> discreto (cant. colores)
+      type: 'quantize', 
+      n: 10,
       scheme: 'ylorbr',
+      label: 'Cantidad de denuncias',
+      legend: true,
     },
     marks: [
-      Plot.density(data, { x: 'lon', y: 'lat', fill: 'density',bandwidth: 2, thresholds: 30 }),
       Plot.geo(barrios, {
-        stroke: 'gray',
+        fill: d => {
+          let nombreBarrio = d.properties.BARRIO
+          let cantReclamos = reclamosPorBarrio.get(nombreBarrio).length
+          return cantReclamos
+        },
+        stroke: '#ccc',
         title: d => `${d.properties.BARRIO}\n${d.properties.DENUNCIAS} denuncias`,
       }),
     ],
     facet: {
       data: data,
-      x: 'categoria',
+      x: d => d3.timeFormat('%a')(d3.timeParse('%a/%d/%y')(d.hora_ingreso)),
     },
-    
+    fx: {
+     // domain: ['lun', 'mar', 'mié', 'jue', 'vie', 'sáb', 'dom']
+    },
     width: 1000
   })
 
