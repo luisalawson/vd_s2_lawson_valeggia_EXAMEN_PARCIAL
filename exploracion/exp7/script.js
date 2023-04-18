13e8d254c9d25d040d0e792dc33ccfa39278d078
luisalawson <luisalawson14@gmail.com>
2023-04-18 10:30:51 -0300

    initial commit

--- exploracion/exp7/script.js
+++ exploracion/exp7/script.js
@@ -1,50 +1,62 @@
-// config. fecha español
-d3.json('https://cdn.jsdelivr.net/npm/d3-time-format@3/locale/es-ES.json').then(locale => {
-  d3.timeFormatDefaultLocale(locale)
-})
-
 const mapaFetch = d3.json('barrios-caba.geojson')
 const dataFetch = d3.dsv(';', 'limpieza_y_recoleccion_filtrado.csv', d3.autoType)
 
 Promise.all([mapaFetch, dataFetch]).then(([barrios, data]) => {
   
-  
-  const reclamosPorBarrio = d3.group(data, d => d.domicilio_barrio) 
+  /* Agrupamos reclamos x barrio */
+  const reclamosPorBarrio = d3.group(data, d => d.domicilio_barrio) // crea un Map
   console.log('reclamosPorBarrio', reclamosPorBarrio)
   
+  /* A cada feature del mapa le agregamos la prop DENUNCIAS */
+  barrios.features.forEach(d => {
+    let nombreBarrio = d.properties.BARRIO
+    let cantReclamos = reclamosPorBarrio.get(nombreBarrio)
+    if (cantReclamos !== undefined) {
+      cantReclamos = cantReclamos.length
+    } else {
+      cantReclamos = 0
+    }
+    d.properties.DENUNCIAS = cantReclamos
+  })
+  
+
+  /* Mapa Coroplético */
   let chartMap = Plot.plot({
-   
+    // https://github.com/observablehq/plot#projection-options
     projection: {
       type: 'mercator',
-      domain: barrios, 
+      domain: barrios, // Objeto GeoJson a encuadrar
     },
     color: {
+      // Quantize continuo (cant. denuncias) -> discreto (cant. colores)
+      type: 'quantize', 
+      n: 10,
       scheme: 'greens',
-      legend: true
+      label: 'Cantidad de denuncias',
+      legend: true,
     },
     marks: [
-     
       Plot.geo(barrios, {
-        fill: d => {
-          let nombreBarrio = d.properties.BARRIO
-          let cantReclamos = reclamosPorBarrio.get(nombreBarrio).length
-          return cantReclamos
-        },
-
+        fill: d => d.properties.DENUNCIAS,
         stroke: 'gray',
         title: d => `${d.properties.BARRIO}\n${d.properties.DENUNCIAS} denuncias`,
       }),
+      Plot.text(
+        barrios.features,
+        Plot.centroid({
+          text: (d) => d.properties.BARRIO,
+          fill: "currentColor",
+          stroke: "white",
+          textAnchor: "center",
+          dx: 4,
+          filter: (d) => d.properties.DENUNCIAS > 80
+        })
+      )
     ],
-    facet: {
-      data: data,
-      x: d => d3.timeFormat('%a')(d3.timeParse('%d/%m/%Y')(d.fecha_ingreso)),
-    },
-    fx: {
-      domain: ['lun', 'mar', 'mié', 'jue', 'vie', 'sáb', 'dom']
-    },
-    width: 1000
   })
 
- 
-  d3.select('#chart_7').append(() => chartMap)
+  /* Agregamos al DOM la visualización chartMap */
+  d3.select('#chart').append(() => chartMap)
+
+
 })
